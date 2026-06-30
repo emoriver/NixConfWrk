8a5517e93df84212d8eaf464476acda78ab308f7
emoriver <emoriver@live.it>
2026-06-30 09:58:36 +0200

    -

--- modules/nixos/services/node-red-packages/default.nix
+++ modules/nixos/services/node-red-packages/default.nix
@@ -6,7 +6,7 @@ pkgs.buildNpmPackage {
 
   src = ./.;
 
-  npmDepsHash = "sha256-LffDLT83JJrCfAs+LeuPmqsFruKJHFx4mDa9UNc9kB0=";
+  npmDepsHash = "sha256-V9Y8SSRbPIxUncQc68Bk/wYXbmAohsYqnuW/6ElcUbs=";
 
   dontNpmBuild = true;
 }
