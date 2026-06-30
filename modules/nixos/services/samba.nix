48f686d7061818b523b591e9f18910e17d52fd6e
Andrea Riva <emoriver@live.it>
2026-06-30 17:40:16 +0200

    -

--- modules/nixos/services/samba.nix
+++ modules/nixos/services/samba.nix
@@ -50,7 +50,7 @@ in
           "netbios name" = cfg.netbiosName;
           "security" = "user";
           #"hosts allow" = "192.168. 127.0.0.1 localhost";
-          "hosts deny" = "0.0.0.0/0";
+          #"hosts deny" = "0.0.0.0/0";
           "guest account" = "nobody";
           "map to guest" = "bad user";
         };
