af5eb26ad4cd89122aca645e88dfca3a4e1fb94d
emoriver <emoriver@live.it>
2026-07-01 16:14:45 +0200

    pacchetto dashboard su node-red

--- modules/nixos/services/node-red.nix
+++ modules/nixos/services/node-red.nix
@@ -9,11 +9,14 @@
 
   systemd.services.node-red.preStart = lib.mkAfter ''
     cd /var/lib/node-red
-    if [ ! -d node_modules/node-red-contrib-opcua ] || \
+    if [ ! -d "node_modules/@opcua/for-node-red" ] || \
+       [ ! -d "node_modules/node-red-contrib-modbus" ] || \
+       [ ! -d "node_modules/node-red-dashboard" ] || \
        [ ! -d "node_modules/@node-red-contrib-themes" ]; then
       ${pkgs.nodejs}/bin/npm install --prefix /var/lib/node-red \
         "@opcua/for-node-red" \
         "node-red-contrib-modbus" \
+        "node-red-dashboard" \
         "@node-red-contrib-themes/theme-collection@4" \
         --no-save --loglevel error
     fi
